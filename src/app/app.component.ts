2ed706820df2dab427f2e1a351837d087fd43c69
S.HASSOUN <serge.hassoun@capgemini.com>
2024-01-20 08:38:29 +0100

    add eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -92,7 +92,7 @@ export class CompanyLogoRenderer implements ICellRendererAngularComp {
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   themeClass = 'ag-theme-material';
